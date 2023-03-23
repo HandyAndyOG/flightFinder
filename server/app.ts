1711e0d6c0d7e854f66e94948180c81e20294244
Andreas Cross <andreasocross@yahoo.com>
2023-03-23 15:33:16 +0100

    cors issue

--- server/app.ts
+++ server/app.ts
@@ -6,14 +6,14 @@ import { Flight, Users } from "./model/model";
 import jwt, { VerifyErrors } from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import { v4 as uuidv4 } from "uuid";
-import cors from "cors";
+
 
 dotenv.config();
 const app: Application = express();
 const bp = require("body-parser");
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: true }));
-app.use(cors());
+
 app.use(function(_: Request, res: Response, next: NextFunction) {
   res.header("Access-Control-Allow-Origin", `${process.env.FRONT_URL}`);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
